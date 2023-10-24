72a8ac306719a6505473fdceb85a31dbf7a2f8fd
Mohammad Hasim Shaikh <hasims2001@gmail.com>
2023-10-24 13:04:45 +0530

    prompt udpated

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const app = express();
-app.use(cors({ origin: 'http://localhost:3000',
+app.use(cors({ origin: process.env.DEPLOYED_LINK,
 optionsSuccessStatus: 200 }));
 app.use(express.json());
 
@@ -24,6 +24,8 @@ app.post("/textgenerate", async (req, res)=>{
               {
                 "role": "system",
                 "content": "act as human, generates creative and contextually relevant text. Your task is to produce(output) human-like text in response of user input without adding extra information. \nThe conversation should look like two humans are talking.\nfor example: `user: what do you think about react? output: To be honest I didn't work on react but i know about react little bit like it is javascript library. built by the facebook and come with lots of feature like hooks, performance optimization concepts, virtual dom and so on.`",
+              },
+              {
                 "role": "user",
                 "content": query
               }
@@ -51,6 +53,8 @@ app.post("/textsummariz", async (req, res)=>{
               {
                 "role": "system",
                 "content": "act as summarization tool that allows users to input lengthy documents, articles, or texts and deliver concise summaries. The tool should be capable of summarizing text from various domains. \nYour task is to summarize the user input in very short form and it contain all the important points without adding extra information..",
+              },
+              {
                 "role": "user",
                 "content": query
               }
@@ -78,6 +82,8 @@ app.post("/languagetranslation", async (req, res)=>{
               {
                 "role": "system",
                 "content": "act as language translate expert, your task is convert user inputed text into user define language without adding extra information.",
+              },
+              {
                 "role": "user",
                 "content": query
               }
